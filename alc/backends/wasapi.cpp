3c8ac41556db1f786fe40aff67ea90567d55d375
John Preston <johnprestonmail@gmail.com>
2024-01-24 17:07:51 +0400

    Always enumerate devices on open.

--- alc/backends/wasapi.cpp
+++ alc/backends/wasapi.cpp
@@ -756,7 +756,7 @@ void WasapiPlayback::open(const char *name)
     {
         if(name)
         {
-            if(PlaybackDevices.empty())
+            //if(PlaybackDevices.empty())
                 pushMessage(MsgType::EnumeratePlayback);
             if(std::strncmp(name, DevNameHead, DevNameHeadLen) == 0)
             {
@@ -1415,7 +1415,7 @@ void WasapiCapture::open(const char *name)
     {
         if(name)
         {
-            if(CaptureDevices.empty())
+            //if(CaptureDevices.empty())
                 pushMessage(MsgType::EnumerateCapture);
             if(std::strncmp(name, DevNameHead, DevNameHeadLen) == 0)
             {
