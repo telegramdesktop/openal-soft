a5342791d9e52b0707b90a7c7f6dc9e73df6531f
John Preston <johnprestonmail@gmail.com>
2024-01-24 17:05:45 +0400

    [coreaudio] Allow opening devices by UID on macOS.

--- alc/backends/coreaudio.cpp
+++ alc/backends/coreaudio.cpp
@@ -112,6 +112,7 @@ struct FourCCPrinter {
 #if CAN_ENUMERATE
 struct DeviceEntry {
     AudioDeviceID mId;
+    std::string mUID;
     std::string mName;
 };
 
@@ -195,6 +196,28 @@ std::string GetDeviceName(AudioDeviceID devId)
     return devname;
 }
 
+std::string GetDeviceUID(AudioDeviceID devId)
+{
+    std::string devuid;
+    CFStringRef uidRef;
+
+    OSStatus err{GetDevProperty(devId, kAudioDevicePropertyDeviceUID, false, 0,
+        sizeof(uidRef), &uidRef)};
+    if (err == noErr)
+    {
+        const CFIndex propSize{CFStringGetMaximumSizeForEncoding(CFStringGetLength(uidRef),
+            kCFStringEncodingUTF8)};
+        devuid.resize(static_cast<size_t>(propSize)+1, '\0');
+
+        CFStringGetCString(uidRef, &devuid[0], propSize+1, kCFStringEncodingUTF8);
+        CFRelease(uidRef);
+    }
+
+    while(!devuid.back())
+        devuid.pop_back();
+    return devuid;
+}
+
 UInt32 GetDeviceChannelCount(AudioDeviceID devId, bool isCapture)
 {
     UInt32 propSize{};
@@ -248,7 +271,7 @@ void EnumerateDevices(std::vector<DeviceEntry> &list, bool isCapture)
 
     if(defaultId != kAudioDeviceUnknown)
     {
-        newdevs.emplace_back(DeviceEntry{defaultId, GetDeviceName(defaultId)});
+        newdevs.emplace_back(DeviceEntry{defaultId, GetDeviceUID(defaultId), GetDeviceName(defaultId)});
         const auto &entry = newdevs.back();
         TRACE("Got device: %s = ID %u\n", entry.mName.c_str(), entry.mId);
     }
@@ -265,7 +288,7 @@ void EnumerateDevices(std::vector<DeviceEntry> &list, bool isCapture)
         auto numChannels = GetDeviceChannelCount(devId, isCapture);
         if(numChannels > 0)
         {
-            newdevs.emplace_back(DeviceEntry{devId, GetDeviceName(devId)});
+            newdevs.emplace_back(DeviceEntry{devId, GetDeviceUID(devId), GetDeviceName(devId)});
             const auto &entry = newdevs.back();
             TRACE("Got device: %s = ID %u\n", entry.mName.c_str(), entry.mId);
         }
@@ -400,7 +423,7 @@ void CoreAudioPlayback::open(std::string_view name)
             EnumerateDevices(PlaybackList, false);
 
         auto find_name = [name](const DeviceEntry &entry) -> bool
-        { return entry.mName == name; };
+        { return entry.mName == name || entry.mUID == name; };
         auto devmatch = std::find_if(PlaybackList.cbegin(), PlaybackList.cend(), find_name);
         if(devmatch == PlaybackList.cend())
             throw al::backend_exception{al::backend_error::NoDevice,
@@ -751,7 +774,7 @@ void CoreAudioCapture::open(std::string_view name)
             EnumerateDevices(CaptureList, true);
 
         auto find_name = [name](const DeviceEntry &entry) -> bool
-        { return entry.mName == name; };
+        { return entry.mName == name || entry.mUID == name; };
         auto devmatch = std::find_if(CaptureList.cbegin(), CaptureList.cend(), find_name);
         if(devmatch == CaptureList.cend())
             throw al::backend_exception{al::backend_error::NoDevice,
